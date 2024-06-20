49502bbb726d27d8cbea06a1e56395419ecd316e
Connor McCarroll <connor@mccarroll.dev>
2024-06-20 18:53:18 +0100

    Fix mobile header missing logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,16 +14,17 @@
 	</aside>
 {/if}
 
-<AppBar>
-	<img src="/logo.svg" alt="Logo" class="h-12" />
-	<svelte:fragment slot="trail">
-		<nav class="list-nav flex w-full items-stretch justify-end">
-			<a href="/about">About</a>
-			<a href="/services">Services</a>
-			<a href="/careers">Careers</a>
-			<a href="/contact">Contact</a>
-		</nav>
-	</svelte:fragment>
+<AppBar slotDefault="mx-auto sm:mx-0 flex flex-col sm:flex-row items-center" gridColumns="1">
+	<a href="/" class="mx-auto my-3 sm:mx-0 sm:my-0 sm:inline-block">
+		<img src="/logo.svg" alt="Logo" class="h-8" />
+	</a>
+
+	<nav class="list-nav flex sm:inline-flex sm:flex-1 sm:justify-end">
+		<a href="/about">About</a>
+		<a href="/services">Services</a>
+		<a href="/careers">Careers</a>
+		<a href="/contact">Contact</a>
+	</nav>
 </AppBar>
 
 <slot></slot>
