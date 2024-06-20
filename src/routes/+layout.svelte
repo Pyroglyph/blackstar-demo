c398983ad55507c6b2d93a29095f353bed67b1fb
Connor McCarroll <connor@mccarroll.dev>
2024-06-20 16:45:42 +0100

    Move alert and top bar to main layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,30 @@
 <script>
 	import '../app.css';
+	import { AppBar } from '@skeletonlabs/skeleton';
+
+	let dismissedAlert = false;
 </script>
+
+{#if !dismissedAlert}
+	<aside class="alert variant-soft-warning">
+		<div class="flex w-full flex-row items-center justify-between">
+			<span>⚠️ Hey! This is a demo site, and therefore does not represent a real company.</span>
+			<button on:click={() => (dismissedAlert = true)}>✕</button>
+		</div>
+	</aside>
+{/if}
+
+<AppBar>
+	<img src="/logo.svg" alt="Logo" class="h-12" />
+	<svelte:fragment slot="trail">
+		<nav class="list-nav flex w-full items-stretch justify-end">
+			<a href="/about">About</a>
+			<a href="/services">Services</a>
+			<a href="/careers">Careers</a>
+			<a href="/contact">Contact</a>
+		</nav>
+	</svelte:fragment>
+</AppBar>
 
 <slot></slot>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,6 @@
 <script lang="ts">
-	import { AppBar } from '@skeletonlabs/skeleton';
-
-	let dismissedAlert = false;
+	
 </script>
-
-{#if !dismissedAlert}
-	<aside class="alert variant-soft-warning">
-		<div class="flex w-full flex-row items-center justify-between">
-			<span>⚠️ Hey! This is a demo site, and therefore does not represent a real company.</span>
-			<button on:click={() => (dismissedAlert = true)}>✕</button>
-		</div>
-	</aside>
-{/if}
-
-<AppBar>
-	<img src="/logo.svg" alt="Logo" class="h-12" />
-	<svelte:fragment slot="trail">
-		<nav class="list-nav flex items-stretch justify-end w-full">
-			<a href="/about">About</a>
-			<a href="/services">Services</a>
-			<a href="/careers">Careers</a>
-			<a href="/contact">Contact</a>
-		</nav>
-	</svelte:fragment>
-</AppBar>
 
 <div class="container mx-auto my-16">
 	<div class="mx-auto my-16 w-48 text-center lg:my-32">
