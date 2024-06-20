66519165519d4fadee611a45c2d9a05a2a8f7753
Connor McCarroll <connor@mccarroll.dev>
2024-06-20 18:13:37 +0100

    Add About page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,54 @@
+<div class="max-w-screen-sm mx-4 my-8 sm:mx-auto">
+	<h1 class="h1 my-8">About</h1>
+	<div class="my-8 sm:flex sm:items-stretch">
+		<p class="mb-4 sm:w-8/12">
+			At Blackstar International, we are a forward-thinking organization committed to driving
+			innovation and excellence in the realm of dynamic synergy. Our team of dedicated professionals
+			is passionate about pushing the boundaries of what is possible, and our unwavering commitment
+			to quality and customer satisfaction sets us apart from the competition.
+		</p>
+		<img
+			class="aspect-square flex-auto object-cover sm:ml-4 sm:w-4/12 rounded-lg"
+			src="https://images.unsplash.com/photo-1522075782449-e45a34f1ddfb?q=80&w=640&auto=format&fit=crop"
+			alt="Woman"
+		/>
+	</div>
+	<div class="my-8 sm:flex sm:items-stretch">
+		<p class="mb-4 sm:order-1 sm:w-8/12">
+			With a rich history of pioneering breakthroughs, we have established ourselves as a leader in
+			the vanguard of transformative solutions, and our cutting-edge approach has earned us a
+			reputation for game-changing initiatives. Our unique blend of strategic thinking,
+			collaborative problem-solving, and visionary leadership has enabled us to achieve remarkable
+			milestones.
+		</p>
+		<img
+			class="sm:order-0 aspect-square flex-auto object-cover sm:mr-4 sm:w-4/12 rounded-lg"
+			src="https://images.unsplash.com/photo-1605712916066-e143c317df72?q=80&w=640&auto=format&fit=crop"
+			alt="Jigsaw piece"
+		/>
+	</div>
+	<div class="my-8 sm:flex sm:items-stretch">
+		<p class="mb-4 sm:w-8/12">
+			As we continue to evolve and grow, we remain dedicated to our core values of integrity,
+			teamwork, and innovation. Our goal is to make a meaningful impact on the global landscape and
+			shape the future of business.
+		</p>
+		<img
+			class="aspect-square flex-auto object-cover sm:ml-4 sm:w-4/12 rounded-lg"
+			src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?q=80&w=640&auto=format&fit=crop"
+			alt="Earth"
+		/>
+	</div>
+	<div class="my-8 sm:flex sm:items-stretch">
+		<p class="mb-4 sm:order-1 sm:w-8/12">
+			At Blackstar International, we are proud of our heritage and excited about the opportunities
+			that lie ahead. We invite you to learn more about our flagship initiative, the Aurora Project,
+			and discover how we can help you navigate the complexities of the modern business landscape.
+		</p>
+		<img
+			class="sm:order-0 aspect-square flex-auto object-cover sm:mr-4 sm:w-4/12 rounded-lg"
+			src="https://images.unsplash.com/photo-1490698900541-76d9b74bdcac?q=80&w=640&auto=format&fit=crop"
+			alt="Navigation"
+		/>
+	</div>
+</div>
