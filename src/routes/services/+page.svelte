7418d3803b3426d46c6b03974c880dd4a77c1127
Connor McCarroll <connor@mccarroll.dev>
2024-06-23 15:22:01 +0100

    Add colourful underlines to services

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -2,45 +2,45 @@
 	<h1 class="h1 my-8">Services</h1>
 	<div class="grid grid-cols-1 gap-6 sm:grid-cols-2">
 		<section>
-			<h2 class="h2 mb-3">Strategy and Planning</h2>
+			<h3 class="h3 mb-3 blue">Strategy and Planning</h3>
 			<p>
 				Chart a course for success with our expert strategy and planning services. We'll help you
 				set goals, prioritize initiatives, and drive results.
 			</p>
 		</section>
 		<section>
-			<h2 class="h2 mb-3">Technology and IT</h2>
+			<h3 class="h3 mb-3 red">Technology and IT</h3>
 			<p>
 				Transform your business with our cutting-edge technology expertise. From cloud migration to
 				cybersecurity, we've got you covered.
 			</p>
 		</section>
 		<section>
-			<h2 class="h2 mb-3">Project Management</h2>
+			<h3 class="h3 mb-3 pink">Project Management</h3>
 			<p>
 				Get projects done right with our expert project management services. From planning to
 				execution, we'll help you deliver on time and on budget.
 			</p>
 		</section>
 		<section>
-			<h2 class="h2 mb-3">Research and Development</h2>
+			<h3 class="h3 mb-3 blue">Business Intelligence</h3>
 			<p>
-				Drive business growth with our R&D solutions. We'll help you identify opportunities, develop
-				new products, and bring them to market quickly and efficiently.
+				Unlock the power of your data with our business intelligence expertise. From data analysis
+				to visualization, we'll help you unlock the power of your data.
 			</p>
 		</section>
 		<section>
-			<h2 class="h2 mb-3">Supply Chain Management</h2>
+			<h3 class="h3 mb-3 red">Supply Chain Management</h3>
 			<p>
 				Elevate your supply chain strategy with our strategic consulting, implementation, and
 				optimization services, ensuring a competitive edge in the market.
 			</p>
 		</section>
 		<section>
-			<h2 class="h2 mb-3">Business Intelligence</h2>
+			<h3 class="h3 mb-3 pink">Research and Development</h3>
 			<p>
-				Unlock the power of your data with our business intelligence expertise. From data analysis
-				to visualization, we'll help you unlock the power of your data.
+				Drive business growth with our R&D solutions. We'll help you identify opportunities, develop
+				new products, and bring them to market quickly and efficiently.
 			</p>
 		</section>
 	</div>
@@ -55,3 +55,25 @@
 		</a>
 	</div>
 </div>
+
+<style lang="less">
+	.h3 {
+		// display: inline-block;
+		// width: fit-content;
+	}
+
+	.h3::after {
+		@apply relative mt-3 block h-1 w-full bg-gradient-to-r;
+		content: '';
+
+		&.blue {
+			@apply from-blue-500 to-cyan-300;
+		}
+		&.red {
+			@apply from-red-500 to-yellow-500;
+		}
+		&.pink {
+			@apply from-pink-500 to-violet-500;
+		}
+	}
+</style>
