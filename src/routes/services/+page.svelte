a0e43452c4ea90a75e4ba9161be63f9416f2acf2
Connor McCarroll <connor@mccarroll.dev>
2024-06-23 13:34:19 +0100

    Create Services page

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,4 +1,57 @@
 <div class="mx-4 my-8 max-w-screen-sm sm:mx-auto">
 	<h1 class="h1 my-8">Services</h1>
-	<p>Content</p>
+	<div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+		<section>
+			<h2 class="h2 mb-2">Strategy and Planning</h2>
+			<p>
+				Chart a course for success with our expert strategy and planning services. We'll help you
+				set goals, prioritize initiatives, and drive results.
+			</p>
+		</section>
+		<section>
+			<h2 class="h2 mb-2">Technology and IT</h2>
+			<p>
+				Transform your business with our cutting-edge technology expertise. From cloud migration to
+				cybersecurity, we've got you covered.
+			</p>
+		</section>
+		<section>
+			<h2 class="h2 mb-2">Project Management</h2>
+			<p>
+				Get projects done right with our expert project management services. From planning to
+				execution, we'll help you deliver on time and on budget.
+			</p>
+		</section>
+		<section>
+			<h2 class="h2 mb-2">Research and Development</h2>
+			<p>
+				Drive business growth with our R&D solutions. We'll help you identify opportunities, develop
+				new products, and bring them to market quickly and efficiently.
+			</p>
+		</section>
+		<section>
+			<h2 class="h2 mb-2">Supply Chain Management</h2>
+			<p>
+				Elevate your supply chain strategy with our strategic consulting, implementation, and
+				optimization services, ensuring a competitive edge in the market.
+			</p>
+		</section>
+		<section>
+			<h2 class="h2 mb-2">Business Intelligence</h2>
+			<p>
+				Unlock the power of your data with our business intelligence expertise. From data analysis
+				to visualization, we'll help you unlock the power of your data.
+			</p>
+		</section>
+	</div>
+
+	<div class="card mt-8 p-4 text-center">
+		<div class="flex-1">Ready to take your business to the next level?</div>
+		<a
+			class="variant-filled btn btn-lg mt-4"
+			href="mailto:connor@mccarroll.dev?subject=I like your portfolio!"
+		>
+			Contact us
+		</a>
+	</div>
 </div>
