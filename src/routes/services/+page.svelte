eaa6d53966a32634a6a5134c5fe079d068ee64a4
Connor McCarroll <connor@mccarroll.dev>
2024-06-23 14:54:11 +0100

    Increase spacing between services

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -1,43 +1,43 @@
 <div class="mx-4 my-8 max-w-screen-sm sm:mx-auto">
 	<h1 class="h1 my-8">Services</h1>
-	<div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+	<div class="grid grid-cols-1 gap-6 sm:grid-cols-2">
 		<section>
-			<h2 class="h2 mb-2">Strategy and Planning</h2>
+			<h2 class="h2 mb-3">Strategy and Planning</h2>
 			<p>
 				Chart a course for success with our expert strategy and planning services. We'll help you
 				set goals, prioritize initiatives, and drive results.
 			</p>
 		</section>
 		<section>
-			<h2 class="h2 mb-2">Technology and IT</h2>
+			<h2 class="h2 mb-3">Technology and IT</h2>
 			<p>
 				Transform your business with our cutting-edge technology expertise. From cloud migration to
 				cybersecurity, we've got you covered.
 			</p>
 		</section>
 		<section>
-			<h2 class="h2 mb-2">Project Management</h2>
+			<h2 class="h2 mb-3">Project Management</h2>
 			<p>
 				Get projects done right with our expert project management services. From planning to
 				execution, we'll help you deliver on time and on budget.
 			</p>
 		</section>
 		<section>
-			<h2 class="h2 mb-2">Research and Development</h2>
+			<h2 class="h2 mb-3">Research and Development</h2>
 			<p>
 				Drive business growth with our R&D solutions. We'll help you identify opportunities, develop
 				new products, and bring them to market quickly and efficiently.
 			</p>
 		</section>
 		<section>
-			<h2 class="h2 mb-2">Supply Chain Management</h2>
+			<h2 class="h2 mb-3">Supply Chain Management</h2>
 			<p>
 				Elevate your supply chain strategy with our strategic consulting, implementation, and
 				optimization services, ensuring a competitive edge in the market.
 			</p>
 		</section>
 		<section>
-			<h2 class="h2 mb-2">Business Intelligence</h2>
+			<h2 class="h2 mb-3">Business Intelligence</h2>
 			<p>
 				Unlock the power of your data with our business intelligence expertise. From data analysis
 				to visualization, we'll help you unlock the power of your data.
